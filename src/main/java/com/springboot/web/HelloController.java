6fd477bdaaf25e83b463344a45c987cc032b12d0
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-05-10 22:29:55 +0800

    提交分支

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,4 +1,11 @@
 package com.springboot.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    private  void  test(){
+        System.out.print("hello world");
+    }
 }
