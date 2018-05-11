bd307a9a6be9b20525c6916064ec113f35b5754e
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-05-11 10:01:26 +0800

    spring mvc搭建成功

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,11 +1,12 @@
 package com.springboot.web;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private  void  test(){
-        System.out.print("hello world");
-    }
+   @RequestMapping("/hello")
+   public String hello(){
+      return "hello";
+   }
 }
