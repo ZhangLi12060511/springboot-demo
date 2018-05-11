f01dced8d6e3721ae432b1427ba4acc9769d4e23
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-05-11 14:26:11 +0800

    增加一个日志配置

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.springboot.web;
 
 import io.swagger.annotations.ApiOperation;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+   private static final Logger logger = Logger.getLogger(HelloController.class);
    @ApiOperation("hello world")
    @RequestMapping("/hello")
    public String hello(){
+      logger.info("hello 测试日志");
       return "hello";
    }
 }
