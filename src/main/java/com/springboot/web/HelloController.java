334528eec95b8a9b245833c901ded8333223d141
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-05-11 14:02:34 +0800

    update code

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.springboot.web;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+   @ApiOperation("hello world")
    @RequestMapping("/hello")
    public String hello(){
       return "hello";
