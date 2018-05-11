955ce4813ba4b66a746740a2eb954fcd62485345
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-05-11 15:19:49 +0800

    增加spring boot 自由的日志系统 配置

--- src/main/java/com/springboot/web/HelloController.java
+++ src/main/java/com/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.springboot.web;
 
 import io.swagger.annotations.ApiOperation;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-   private static final Logger logger = Logger.getLogger(HelloController.class);
+   private Logger logger = LoggerFactory.getLogger(this.getClass());
    @ApiOperation("hello world")
    @RequestMapping("/hello")
    public String hello(){
-      logger.info("hello 测试日志");
       return "hello";
    }
 }
