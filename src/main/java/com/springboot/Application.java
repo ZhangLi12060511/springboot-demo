b8501cd22ae54f445f7696621a9fcb01470edd3c
ZhangLi12060511 <ZhangLi12060511@163.com>
2018-06-07 18:44:41 +0800

    èupdate

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -3,7 +3,6 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author zhangbenben
@@ -11,6 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class,args);
     }
 }
